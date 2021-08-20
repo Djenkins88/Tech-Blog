262461657e81d7d6194956ac3018d551215f5e5d
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-08-20 18:45:10 -0500

    filled out seeds files

---
+++ seeds/seeds.js
@@ -0,0 +1,37 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const collectionData = require('./collectionData.json');
+const itemData = require('./itemData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const collection of collectionData) {
+    await Collection.create({
+      ...collection,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+  
+  const colData = await Collection.findAll({});
+
+  const collect = colData.map((col) => col.get({ plain: true }));
+
+  for (const item of itemData) {
+    await Item.create({
+      ...item,
+      collection_id: collect[Math.floor(Math.random() * collect.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
